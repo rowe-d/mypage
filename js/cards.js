84fe074e1b59acab30415e3f08315a532ab73954
rowe-d <rowed@live.co.uk>
2016-02-16 09:06:29 +0000

    no fade

--- js/cards.js
+++ js/cards.js
@@ -3,9 +3,9 @@ $(function() {
       var $currentCard = $(this.parentElement.parentElement);
        var _prev = $currentCard.find(".card-step.current").prev();
        _prev = _prev.length == 0 ? $currentCard.find(".card-step:last") : _prev;
-           $currentCard.find(".card-step.current").fadeOut(300, function() {
+           $currentCard.find(".card-step.current").fadeOut(0, function() {
                $(this).removeClass("current");
-                _prev.fadeIn(300, function() {
+                _prev.fadeIn(0, function() {
                     $(this).addClass("current");
                 });
            });
@@ -15,9 +15,9 @@ $(function() {
       var $currentCard = $(this.parentElement.parentElement);
        var _next =  $currentCard.find(".card-step.current").next();
       _next = _next.length == 0 ? $currentCard.find(".card-step:first") : _next;
-           $currentCard.find(".card-step.current").fadeOut(300, function() {
+           $currentCard.find(".card-step.current").fadeOut(0, function() {
                 $(this).removeClass("current");
-                _next.fadeIn(300, function() {
+                _next.fadeIn(0, function() {
                     $(this).addClass("current");
                 });
            });
